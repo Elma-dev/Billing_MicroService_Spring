86044235a71616f99b013b5fd1aa0dc51d0b9a34
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-28 18:48:09 +0100

    commit

--- src/main/java/dev/elma/billing_microservice_spring/BillingMicroServiceSpringApplication.java
+++ src/main/java/dev/elma/billing_microservice_spring/BillingMicroServiceSpringApplication.java
@@ -1,13 +1,28 @@
 package dev.elma.billing_microservice_spring;
 
+import dev.elma.billing_microservice_spring.entities.Bill;
+import dev.elma.billing_microservice_spring.repositories.BillingRepository;
+import dev.elma.billing_microservice_spring.repositories.ProductItemRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BillingMicroServiceSpringApplication {
+import java.util.Date;
 
+@SpringBootApplication
+@AllArgsConstructor
+public class BillingMicroServiceSpringApplication implements CommandLineRunner {
+    private BillingRepository billingRepository;
+    private ProductItemRepository productItemRepository;
     public static void main(String[] args) {
         SpringApplication.run(BillingMicroServiceSpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Bill.builder()
+                .billingDate(new Date()).customerID(Long.valueOf(1)).build();
+
+    }
 }
--- src/main/java/dev/elma/billing_microservice_spring/repositories/ProductItemRepository.java
+++ src/main/java/dev/elma/billing_microservice_spring/repositories/ProductItemRepository.java
@@ -1,9 +1,13 @@
 package dev.elma.billing_microservice_spring.repositories;
 
+import dev.elma.billing_microservice_spring.entities.Bill;
 import dev.elma.billing_microservice_spring.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ProductItemRepository extends JpaRepository<ProductItem,Long> {
+    List<ProductItem> findProductItemByBillId(Long id);
 }

097d497f50bbea9c5294db25a1aa6ece7d1ae98c
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-28 16:50:48 +0100

    Initial commit

---
+++ src/main/java/dev/elma/billing_microservice_spring/BillingMicroServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.billing_microservice_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingMicroServiceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingMicroServiceSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/billing_microservice_spring/BillingMicroServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.billing_microservice_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingMicroServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
